e2dfbf3361382087e1fa1e70d6828fbeb84548cd
Suncodev <suncodev@gmail.com>
2022-10-02 17:32:15 +0700

    Constructor Java

---
+++ Constructor.java
@@ -0,0 +1,34 @@
+class Mahasiswa {
+
+    String nama;
+    String kelas;
+    String nim;
+    String prodi;
+
+    // Constructor Code
+
+    Mahasiswa (String Inputnama, String Inputkelas, String Inputnim, String Inputprodi) {
+
+        nama = Inputnama;
+        kelas = Inputkelas;
+        nim = Inputnim;
+        prodi = Inputprodi;
+
+        System.out.println(nama);
+        System.out.println(kelas);
+        System.out.println(nim);
+        System.out.println(prodi);
+
+    }
+
+}
+
+public class Constructor {
+
+    public static void main (String[] args ) {
+
+        Mahasiswa datamahasiswa = new Mahasiswa("Nizar Fadilah", "Information D", "2211003322", "Information");
+    }
+
+
+}
